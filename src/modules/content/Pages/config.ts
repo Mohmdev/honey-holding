0f6be99ecea5b8e2e556882ce4f9be16955dadb5
Mohmdev <Mohammadr_m@outlook.com>
2024-12-14 10:43:38 +0300

    fix: update collection parameter in generatePreviewPath for Pages and Posts configurations

--- src/modules/content/Pages/config.ts
+++ src/modules/content/Pages/config.ts
@@ -43,7 +43,7 @@ export const Pages: CollectionConfig<'pages'> = {
       url: ({ data, req }) => {
         const path = generatePreviewPath({
           slug: typeof data?.slug === 'string' ? data.slug : '',
-          collection: 'posts',
+          collection: 'pages',
           req,
         })
 
